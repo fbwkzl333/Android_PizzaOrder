772ec89d23b565393add29b26d64f1b60728937e
fbwkzl333 <fbwkzl111@naver.com>
2016-11-25 20:06:42 +0900

    MainActivity.java 주석 작성

--- app/src/main/java/com/example/donghee/android1125/MainActivity.java
+++ app/src/main/java/com/example/donghee/android1125/MainActivity.java
@@ -12,12 +12,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edit1, edit2, edit3;
-    CheckBox chB1;
-    RadioButton Rdio1, Rdio2;
-    ImageView image;
-    Button bt1;
-    TextView Tview1, Tview2, Tview3;
+    EditText edit1, edit2, edit3;   // 입력창 3개
+    CheckBox chB1;  // 체크박스
+    RadioButton Rdio1, Rdio2;   // 라디오 버튼 2개
+    ImageView image;    // 이미지뷰
+    Button bt1; // '주문완료' 버튼
+    TextView Tview1, Tview2, Tview3;    // 주문개수, 주문금액, 소스선택 여부 출력. 텍스트뷰
+    String num1, num2, num3;    // 값 계산용 변수 (피자, 스파게티, 샐러디)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,29 +51,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
         bt1.setOnClickListener(new View.OnClickListener() {  // 주문완료 버튼 클릭 이벤트
             @Override
             public void onClick(View v) {
-                String num1 = edit1.getText().toString();
-                String num2 = edit2.getText().toString();
-                String num3 = edit3.getText().toString();
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                num3 = edit3.getText().toString();      // 입력값 String 형식으로 num에 담음
 
-                int result1 = Integer.parseInt(num1) + Integer.parseInt(num2) + Integer.parseInt(num3);
-                Double result2 = (Double.parseDouble(num1) * 16000) + ((Double.parseDouble(num2)) * 11000) + ((Double.parseDouble(num3)) * 4000);
+                int result1 = Integer.parseInt(num1) + Integer.parseInt(num2) + Integer.parseInt(num3);     // 입력창1~3 입력값 전부 더함(Int). 주문 개수
+                Double result2 = (Double.parseDouble(num1) * 16000) + ((Double.parseDouble(num2)) * 11000) + ((Double.parseDouble(num3)) * 4000);   // 주문 금액 구함(Double)
 
                 if(chB1.isChecked()) {      // 멤버쉽 카드 있을경우 7% 할인
                     result2 = (Double)result2 - ((Double)result2 *0.07);
-                } else
+                }
 
-                Tview1.setText("주문 개수 : "+ result1); // 텍스트뷰1 주문 개수
-                Tview2.setText("주문 금액 : "+ result2); // 텍스트뷰2 주문 금액
+                Tview1.setText("주문 개수 : "+ result1); // 텍스트뷰1에 주문 개수 출력
+                Tview2.setText("주문 금액 : "+ result2); // 텍스트뷰2에 주문 금액 출력
 
-                if(Rdio1.isChecked()) { // 소스 선택에 따른 텍스트뷰3
-                    Tview3.setText("피클을 선택하셨습니다.");
-                } else {
+                if(Rdio1.isChecked()) { // 라디오버튼1 (피클) 이 체크가 되있다면
+                    Tview3.setText("피클을 선택하셨습니다."); // 텍스트뷰3 에 보여줄 값
+                } else {    // 라디오버튼1 (피클) 이 체크가 안되있다면
                     Tview3.setText("소스를 선택하셨습니다.");
                 }
 
